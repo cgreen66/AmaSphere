e0416d7027d5905293d8b14166ff3b23670b14fd
Christopher Green <cg2648@nyu.edu>
2024-01-12 17:18:07 -0500

    frontend start

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store/store';
+import { csrfFetch, restoreCSRF } from './store/csrf';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  console.log("Store attached to window:", window.store); // Add this line
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
